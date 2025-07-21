064b3960a2d20be04a5f8afe96ded57c8b16c28f
netsi-YD <netsanetyiblet12@gmail.com>
2025-07-21 23:02:55 +0300

    added a function for each of the 4 basic math operation

---
+++ script.js
@@ -0,0 +1,13 @@
+function add(a,b){
+return a+b;
+}
+function sub(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide (a,b){
+    if(b==0){return "divisor can't be zero!"}
+    return a/b;
+}
