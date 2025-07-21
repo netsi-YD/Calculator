d0c7d67308f0972247a431b7c6aa49c0a74c77e4
netsi-YD <netsanetyiblet12@gmail.com>
2025-07-21 23:06:59 +0300

    added an operate function using switch statement

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function add(a,b){
 return a+b;
 }
-function sub(a,b){
+function subtract(a,b){
     return a-b;
 }
 function multiply(a,b){
@@ -11,3 +11,20 @@ function divide (a,b){
     if(b==0){return "divisor can't be zero!"}
     return a/b;
 }
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+    default:
+      return null;
+  }
+}
+
